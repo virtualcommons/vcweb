694f08e3fce45ddfc78181c55d55063957587ccf
alllee <none@none>
2010-07-09 16:15:08 -0700

    testing emails again and removing abstract ConfigurationParameter from admin interface

--- vcweb/core/admin.py
+++ vcweb/core/admin.py
@@ -8,13 +8,12 @@ from django.contrib import admin
 from vcweb.core.models import *
 
 admin.site.register(GameMetadata)
-admin.site.register(ConfigurationParameter)
 admin.site.register(DataParameter)
 admin.site.register(RoundParameter)
 admin.site.register(GameConfiguration)
 admin.site.register(RoundConfiguration)
 admin.site.register(Experimenter)
+admin.site.register(Participant)
 admin.site.register(Group)
 admin.site.register(GameInstance)
-admin.site.register(Participant)
 
