0297194df3fb835faf31c5788591057c73ae7c22
Allen Lee <allen.lee@asu.edu>
2010-09-18 00:46:49 -0700

    finished simple/naive participant registration

--- vcweb/core/forms.py
+++ vcweb/core/forms.py
@@ -15,8 +15,6 @@ class RegistrationForm(forms.Form):
     email = forms.EmailField(required=True, widget=widgets.TextInput(attrs=REQUIRED_EMAIL_ATTRIBUTES))
     password = forms.CharField(required=True, widget=widgets.PasswordInput(attrs=REQUIRED_ATTRIBUTES))
     confirm_password = forms.CharField(required=True, widget=widgets.PasswordInput(attrs=REQUIRED_ATTRIBUTES))
-    # these are hidden unless they check "experimenter request"
-    experimenter = forms.BooleanField()
     first_name = forms.CharField(required=True, widget=widgets.TextInput(attrs=REQUIRED_ATTRIBUTES))
     last_name = forms.CharField(required=True, widget=widgets.TextInput(attrs=REQUIRED_ATTRIBUTES))
     institution = forms.CharField(required=True, widget=widgets.TextInput(attrs=REQUIRED_ATTRIBUTES))
--- vcweb/core/views.py
+++ vcweb/core/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 from django.shortcuts import render_to_response, redirect
 from django.template.context import RequestContext
 from vcweb.core.forms import RegistrationForm, LoginForm
-from vcweb.core.models import Participant, Experiment, Experimenter
+from vcweb.core.models import Participant, Experiment, Experimenter, Institution
 import logging
 
 logger = logging.getLogger("core-views")
@@ -43,17 +43,22 @@ def register(request):
             password = form.cleaned_data['password']
             first_name = form.cleaned_data['first_name']
             last_name = form.cleaned_data['last_name']
-            institution = form.cleaned_data['institution']
+            institution_string = form.cleaned_data['institution']
+            institution = Institution.objects.get_or_create(name=institution_string)
             user = User.objects.create_user(email, email, password)
             user.first_name = first_name
             user.last_name = last_name
             user.save()
-            participant = Participant(institution=institution, user=user)
+            participant = Participant(user=user)
             participant.save()
-            return redirect('core:profile')
+            auth.login(request, auth.authenticate(username=email, password=password))
+            return redirect('core:participant-index')
+        else:
+            logger.debug("form had errors: %s", form.errors)
     else:
         form = RegistrationForm()
-        return render_to_response('registration/register.html', locals(), context_instance=RequestContext(request))
+
+    return render_to_response('registration/register.html', locals(), context_instance=RequestContext(request))
 
 @login_required
 def experimenter_index(request):
@@ -70,7 +75,7 @@ def participant_index(request):
     user = request.user
     try:
         participant = user.participant
-        experiments = [ group.experiment for group in participant.group.all() ]
+        experiments = [ group.experiment for group in participant.groups.all() ]
         return render_to_response('participant-index.html', RequestContext(request, locals()))
     except Participant.DoesNotExist:
         # add error message
