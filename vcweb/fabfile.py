e13342c4ca8b7153b4988c9f84eef63c72e42415
Allen Lee <allen.lee@asu.edu>
2011-01-02 00:59:18 -0700

    refining fabfile a bit to resync databases on dev at least

--- vcweb/fabfile.py
+++ vcweb/fabfile.py
@@ -1,26 +1,45 @@
-from fabric.api import *
+from fabric.api import run, local, sudo, cd, env
+from fabric.decorators import hosts
 from fabric.contrib.console import confirm
 
-env.hosts = ['dev.commons.asu.edu']
 
+@hosts('dev.commons.asu.edu', 'localhost')
 def host_type():
     run('uname -a')
 
 def test():
     local('./manage.py test', capture=False)
 
+syncdb_commands = ['(test -f vcweb.db && rm vcweb.db) || true', 
+        './manage.py syncdb --noinput', 
+        './manage.py loaddata test_users_participants', 
+        './manage.py loaddata forestry_test_data' ]
+
 def syncdb():
-    local('test -f vcweb.db && rm vcweb.db', capture=False)
-    local('./manage.py syncdb --noinput', capture=False)
-    local('./manage.py loaddata test_users_participants', capture=False)
-    local('./manage.py loaddata forestry_test_data', capture=False)
+    for command in syncdb_commands:
+        local(command, capture=False)
+
+def push():
+    local('hg push')
+
+@hosts('dev.commons.asu.edu')
+def dev():
+    deploy()
+
+@hosts('vcweb.asu.edu')
+def prod():
+    deploy()
 
 def deploy():
-    local('hg push')
-    if confirm("Deploy to %s ?" % env.hosts):
+    push()
+    if confirm("Deploy to %s ?" % (dev.hosts or prod.hosts)):
         with cd('/opt/webapps/virtualcommons/'):
             run('hg pull')
             run('hg up')
-            sudo('find . -type d -exec chmod a+x {} \;', pty=True)
-            sudo('chmod -R a+r .', pty=True)
-            sudo('service httpd restart', pty=True)
+            with cd('/opt/webapps/virtualcommons/vcweb'):
+                run('touch django.wsgi')
+                for command in syncdb_commands:
+                    run(command)
+            sudo('chmod -R ug+rw .', pty=True)
+            sudo('find . -type d -exec chmod ug+x {} \;', pty=True)
+            sudo('chown -R alllee:commons .', pty=True)
