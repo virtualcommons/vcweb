baef67e8ad44d571e27956a7373364e37d6fe0cf
Allen Lee <allen.lee@asu.edu>
2011-01-01 17:38:16 -0700

    adding fab deploy to dev.commons

--- vcweb/fabfile.py
+++ vcweb/fabfile.py
@@ -1,7 +1,10 @@
-from fabric.api import run, local
+from fabric.api import *
+from fabric.contrib.console import confirm
+
+env.hosts = ['dev.commons.asu.edu']
 
 def host_type():
-    run('uname -s')
+    run('uname -a')
 
 def test():
     local('./manage.py test', capture=False)
@@ -11,3 +14,13 @@ def syncdb():
     local('./manage.py syncdb --noinput', capture=False)
     local('./manage.py loaddata test_users_participants', capture=False)
     local('./manage.py loaddata forestry_test_data', capture=False)
+
+def deploy():
+    local('hg push')
+    if confirm("Deploy to %s ?" % env.hosts):
+        with cd('/opt/webapps/virtualcommons/'):
+            run('hg pull')
+            run('hg up')
+            sudo('find . -type d -exec chmod a+x {} \;', pty=True)
+            sudo('chmod -R a+r .', pty=True)
+            sudo('service httpd restart', pty=True)
