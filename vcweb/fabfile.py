4a785ff8cdf59b342044140e40eb20e9e44786dc
Allen Lee <allen.lee@asu.edu>
2011-01-04 01:51:55 -0700

    fabfile working again, still needs cleanup

--- vcweb/fabfile.py
+++ vcweb/fabfile.py
@@ -14,32 +14,36 @@ currently only works for sqlite3 development database.  Need to do it by hand wi
 few times to figure out what to automate.
 """
 syncdb_commands = ['(test -f vcweb.db && rm vcweb.db) || true', 
-        './manage.py syncdb --noinput', 
-        './manage.py loaddata test_users_participants', 
-        './manage.py loaddata forestry_test_data' ]
+        '%(python)s manage.py syncdb --noinput' % env, 
+        '%(python)s manage.py loaddata test_users_participants' % env, 
+        '%(python)s manage.py loaddata forestry_test_data' % env]
 
 def syncdb():
-    for command in syncdb_commands:
-        local(command, capture=False)
+    with cd(env.project_path):
+        for command in syncdb_commands:
+            _virtualenv(command)
 
 def virtualenv():
     """ Setup a fresh virtualenv """
     run('virtualenv -p %(python)s --no-site-packages %(virtualenv_path)s;' % env)
 
-def _virtualenv(command):
+def _virtualenv(command, use_django_path=False):
     """ source the virtualenv before executing this command """
+    #env.command = '%s/%s' % (env.project_path, command) if use_django_path else command
     env.command = command
     run('source %(virtualenv_path)s/bin/activate && %(command)s' % env)
 
-def reqs():
-    _virtualenv('pip install -U -r %(project_path)s/requirements.pip' % env)
+def pip():
+    with cd(env.project_path):
+        _virtualenv('pip install -E %(virtualenv_path)s -r %(project_path)s/requirements.pip' % env)
 
 def host_type():
     run('uname -a')
 
 def test():
     env.hosts = ['localhost']
-    _virtualenv('./manage.py test')
+    with cd(env.project_path):
+        _virtualenv('%(python)s manage.py test' % env)
 
 def server(ip="149.169.203.115", port=8080):
     local("./manage.py runserver {ip}:{port}".format(**locals()), capture=False)
@@ -62,14 +66,9 @@ def loc():
 def deploy():
     push()
     if confirm("Deploy to %s ?" % (loc.hosts or dev.hosts or prod.hosts)):
-        with cd(env.deploy_path):
+        with cd(env.project_path):
             run('hg pull')
             run('hg up')
-            with cd(env.project_name):
-                reqs()
-                test()
-                for command in syncdb_commands:
-                    run(command)
             sudo('chmod -R ug+rw .', pty=True)
             sudo('find . -type d -exec chmod ug+x {} \;', pty=True)
             sudo('service httpd restart', pty=True)
