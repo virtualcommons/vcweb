3c5da7cdba984c0f56d2e8ac1823f82779e41972
Allen Lee <allen.lee@asu.edu>
2011-01-16 01:07:11 -0700

    splitting url out for the time being to see if that's causing the weirdness w/ django dev server

--- vcweb/core/urls.py
+++ vcweb/core/urls.py
@@ -11,6 +11,7 @@ urlpatterns = patterns('vcweb.core.views',
     url(r'accounts/profile/$', 'account_profile', name='profile'),
     url(r'experimenter/$', 'experimenter_index', name='experimenter-index'),
     url(r'participant/$', 'participant_index', name='participant-index'),
-    url(r'(?P<experiment_id>\d+)|(?P<experiment_namespace>\w+)/instructions', 'instructions', name='instructions'),
+    url(r'(?P<experiment_id>\d+)/instructions', 'instructions', name='experiment-id-instructions'),
+    url(r'(?P<experiment_namespace>\w+)/instructions', 'instructions', name='instructions'),
     url(r'experimenter/configure/(?P<experiment_id>\d+)$', 'configure', name='configure-experiment'),
 )
