d72b3b91bbb34e3a5b5082d27ed1388a71a35583
Allen Lee <allen.lee@asu.edu>
2011-01-18 16:13:22 -0700

    testing new way to set up ExperimentMetadata records via post_syncdb.

--- vcweb/forestry/management.py
+++ vcweb/forestry/management.py
@@ -1,12 +1,29 @@
 #from django.dispatch import receiver
 
-from django.db.models.signals import post_save
+from django.db.models.signals import post_syncdb
 from vcweb.core import signals
-from vcweb.forestry.models import round_setup, round_ended, get_experiment_metadata
+from vcweb.forestry.models import round_setup, round_ended
 from vcweb.core.models import Experiment, ExperimentMetadata
+
+import vcweb.core.models
 
 import logging
 logger = logging.getLogger(__name__)
+
+'''
+    {
+        "fields": {
+            "about_url": "http://commons.asu.edu", 
+            "description": "Web-based version of the forestry field experiments.", 
+            "namespace": "forestry", 
+            "title": "Forestry Web Experiment",
+            "date_created": "2011-01-01"
+        }, 
+        "model": "core.experimentmetadata", 
+        "pk": 1
+    }, 
+
+'''
 
 #@receiver(signals.round_started, sender='forestry')
 def round_started_handler(sender, experiment_id=None, **kwargs):
@@ -19,10 +36,19 @@ def round_ended_handler(sender, experiment_id=None, **kwargs):
     round_ended(Experiment.objects.get(pk=experiment_id))
 
 
-def post_save_handler(sender, instance=None, **kwargs):
-    if instance.namespace == 'forestry':
-        forestry = get_experiment_metadata()
+def post_syncdb_handler(sender, **kwargs):
+    forestry_dict = {
+            "about_url": "http://commons.asu.edu",
+            "description": "Web-based version of the forestry field experiments.",
+            "namespace": "forestry",
+            "title": "Forestry Web Experiment",
+            "date_created": "2011-01-01"
+            }
+    forestry, created = ExperimentMetadata.objects.get_or_create(**forestry_dict)
+    if created:
         signals.round_started.connect(round_started_handler, sender=forestry.pk)
         signals.round_ended.connect(round_ended_handler, sender=forestry.pk)
+    else:
+        logger.debug("There was an existing forestry ExperimentMetadata experiment")
 
-post_save.connect(post_save_handler, sender=ExperimentMetadata)
+post_syncdb.connect(post_syncdb_handler, sender=vcweb.core.models)
