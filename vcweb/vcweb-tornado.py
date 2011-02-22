2aa31d1066630eb3da88fc35a89acc0468e3c5d5
Allen Lee <allen.lee@asu.edu>
2011-02-21 22:32:45 -0700

    moving to tornadio package

--- vcweb/vcweb-tornado.py
+++ vcweb/vcweb-tornado.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
 import tornado.web
-from tornad_io import SocketIOHandler
-from tornad_io import SocketIOServer
+import tornadio.server
+import tornadio
 
 import os
 import sys
@@ -28,21 +28,21 @@ class SessionManager:
     def get_participant(self, session):
         logger.debug("trying to retrieve participant group relationship for session id %s" % session)
         logger.debug("maps are %s and %s" % (self.session_id_to_participant, self.pgr_to_session))
-        return ParticipantGroupRelationship.objects.get(pk=self.session_id_to_participant[session.id])
+        return ParticipantGroupRelationship.objects.get(pk=self.session_id_to_participant[session])
 
     def add(self, session, participant_group_relationship):
         if participant_group_relationship.pk in self.pgr_to_session:
             logger.debug("participant already has a session, removing previous mappings.")
             self.remove(self.pgr_to_session[participant_group_relationship.pk])
 
-        self.session_id_to_participant[session.id] = participant_group_relationship.pk
+        self.session_id_to_participant[session] = participant_group_relationship.pk
         self.pgr_to_session[participant_group_relationship.pk] = session
 
     def remove(self, session):
         try:
-            participant_group_pk = self.session_id_to_participant[session.id]
+            participant_group_pk = self.session_id_to_participant[session]
             del self.pgr_to_session[participant_group_pk]
-            del self.session_id_to_participant[session.id]
+            del self.session_id_to_participant[session]
         except KeyError, k:
             logger.warning( "caught key error %s while trying to remove session %s" % (session, k) )
             pass
@@ -71,33 +71,24 @@ class IndexHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("chat.html")
 
-''' 
-FIXME: make this a class / instance var on ChatHandler? But then it forces us to
-type ChatHandler.session_manager instead of just session_manager...
+'''
+FIXME: make this a class / instance var ?
 '''
 session_manager = SessionManager()
 
-class ChatHandler(SocketIOHandler):
-    def on_open(self, *args, **kwargs):
-        ''' parse args / kwargs for participant session info so we know which group
-        to route this guy to
-        '''
-        logger.debug("args are: %s" % str(args))
-        logger.debug("kwargs are: %s" % str(kwargs))
-        logger.debug("session is %s" % self.session)
+class ChatHandler(tornadio.SocketConnection):
 
     def on_message(self, message):
         ''' incoming JSON message gets converted to a Python dict via simplejson '''
-        logger.debug("received message %s from handler %s" % (message, self))
-        logger.debug("handler session is %s" % self.session)
         event = to_event(message)
         if event.type == 'connect':
             participant_group_rel = ParticipantGroupRelationship.objects.get(participant__pk=event.participant_id,
                     group__pk=event.group_id)
-            event.message = "<div>Participant %s joined group %s chat.</div>" % (participant_group_rel.participant, participant_group_rel.group)
-            session_manager.add(self.session, participant_group_rel)
+            logger.debug("%s joined group %s chat." % (participant_group_rel.participant, participant_group_rel.group))
+            event.message = "<div>%s joined group %s chat.</div>" % (participant_group_rel.participant_number, participant_group_rel.group)
+            session_manager.add(self, participant_group_rel)
             for participant_group_pk, session in session_manager.sessions(participant_group_rel.group):
-                session['output_handle'].send(event.message)
+                session.send(event.message)
             return
         elif event.type == 'experimenter':
             experiment = Experiment.objects.get(pk=event.experiment_id)
@@ -107,34 +98,34 @@ class ChatHandler(SocketIOHandler):
                   event.message)
             for g in experiment.groups.all():
                for participant_group_pk, session in session_manager.sessions(g):
-                  session['output_handle'].send(event.message)
+                  session.send(event.message)
             return
         else:
             # check session id..
-            participant_group_rel = session_manager.get_participant(self.session)
+            participant_group_rel = session_manager.get_participant(self)
             chat_message = ChatMessage.objects.create(participant_group_relationship=participant_group_rel,
                     message=event.message,
                     round_configuration=participant_group_rel.group.current_round,
                     experiment=participant_group_rel.group.experiment
                     )
             for participant_group_pk, session in session_manager.sessions(participant_group_rel.group):
-                session['output_handle'].send(chat_message.as_html)
+                session.send(chat_message.as_html)
 
     def on_close(self):
         logger.debug("closing %s" % self)
-        session_manager.remove(self.session)
+        session_manager.remove(self)
 
 def main(argv=None):
     if argv is None:
         argv = sys.argv
     # use the routes classmethod to build the correct resource
-    chatRoute = ChatHandler.routes("socket.io/*")
+    chatRouter = tornadio.get_router(ChatHandler, resource="chat")
     #configure the Tornado application
     # currently only allow one command-line argument, the port to run on.
     port = int(argv[1]) if (len(argv) > 1) else 8888
 
     application = tornado.web.Application(
-            [(r'/', IndexHandler), chatRoute],
+            [(r'/', IndexHandler), chatRouter.route(), ],
             enabled_protocols=['websocket', 'flashsocket', 'xhr-multipart', 'xhr-polling'],
             flash_policy_port=8043,
             flash_policy_file='/etc/nginx/flashpolicy.xml',
@@ -142,7 +133,7 @@ def main(argv=None):
             # only needed for standalone testing
             static_path=os.path.join(os.path.dirname(__file__), "static"),
             )
-    return SocketIOServer(application)
+    return tornadio.server.SocketServer(application)
 
 if __name__ == "__main__":
     sys.exit(main())
