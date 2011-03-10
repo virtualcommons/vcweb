e44868afca78b6dc6f0120581f4c53e4fba28e93
Allen Lee <allen.lee@asu.edu>
2011-03-10 12:35:19 -0700

    fixed socket.io communication between participant and experimenters.
    The problem was that the simplejson.dumps being sent from tornadio to the socket.io
    s.on('message') handlers were being treated as strings and need to be
    JSON.parse()ed.

--- vcweb/static/js/common.js
+++ vcweb/static/js/common.js
@@ -1,2 +1,7 @@
 /* SlimConsoleDummy.min.js from https://github.com/andyet/ConsoleDummy.js */
 /*(function(b){function c(){}for(var d=["error","info","log","warn"],a;a=d.pop();)b[a]=b[a]||c})(window.console=window.console={});*/
+if (!window.console) console = {};
+console.log = console.log || function(){};
+console.warn = console.warn || function(){};
+console.error = console.error || function(){};
+console.info = console.info || function(){};
