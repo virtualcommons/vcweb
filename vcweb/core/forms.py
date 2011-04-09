21aa0d73483e15e5ed67058aefacc94ee1e8e0a2
Allen Lee <allen.lee@asu.edu>
2011-04-08 17:17:44 -0700

    slight tooltip tweaking, add registration next.

--- vcweb/core/forms.py
+++ vcweb/core/forms.py
@@ -78,14 +78,14 @@ class EmailListField(forms.CharField):
 
 class RegisterParticipantsForm(forms.ModelForm):
     experiment_pk = forms.IntegerField(widget=widgets.HiddenInput)
-    experiment_passcode = forms.CharField(min_length=3, label="Experiment passcode", help_text='The password used to login to your experiment.', initial='test')
+    experiment_passcode = forms.CharField(min_length=3, label="Experiment passcode", help_text='The password used to login to your experiment.')
     institution_name = forms.CharField(min_length=3,label="Institution name",
             help_text='The name of the institution to be associated with these test participants')
     institution_url = forms.CharField(min_length=3,label='Institution URL',
-            required=False, help_text='A URL, if applicable, for the institution.  E.g., http://www.asu.edu')
+            required=False, help_text='A URL, if applicable, for the institution (e.g., http://www.asu.edu)')
 
 class RegisterSimpleParticipantsForm(RegisterParticipantsForm):
-    email_suffix = forms.CharField(min_length='3', help_text='Generated participants will have usernames of the format s1..sn@<email_suffix>.  For example, if you register 20 participants with an email suffix of example.edu, the system will generate 20 participants with usernames ranging from s1@example.edu, s2@example.edu, up to s20@example.edu.')
+    email_suffix = forms.CharField(min_length='3', help_text='An email suffix without the "@" symbol.  Generated participants will have usernames of the format s1..sn@email_suffix.  For example, if you register 20 participants with an email suffix of example.edu, the system will generate 20 participants with usernames ranging from s1@example.edu, s2@example.edu, s3@example.edu, ... s20@example.edu.')
     number_of_participants = forms.IntegerField(min_value=1, help_text='The number of participants to generate.')
 
 class RegisterEmailListParticipantsForm(RegisterParticipantsForm):
