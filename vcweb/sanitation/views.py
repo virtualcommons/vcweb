65fce2758bfa3a25e849a2970c8ac820c8c81c32
Marcel Hurtado <marcel.hurtado@gmail.com>
2011-06-07 15:48:26 -0700

    Fixed bug in participate getattr based on current_location and adding some initial template stubs for the sanitation game

--- vcweb/sanitation/views.py
+++ vcweb/sanitation/views.py
@@ -5,6 +5,13 @@ from django.template.context import RequestContext
 import logging
 logger = logging.getLogger(__name__)
 
+class QuizQuestion(object):
+	def __init__(self, **kwargs):
+		self.__dict__.update(kwargs)
+
+q1 = QuizQuestion(type="radio", options=['Yes', 'No', 'Maybe'], sequence_number=1, answer="No")
+
+
 def configure(request, experiment_id=None):
     experiment = Experiment.objects.get(pk=experiment_id)
     return render_to_response('sanitation/configure.html', {
@@ -13,31 +20,41 @@ def configure(request, experiment_id=None):
         context_instance=RequestContext(request))
 
 def consent(request, experiment):
-    pass
+	logger.debug("handling consent")
+	return render_to_response('sanitation/consent.html', locals(), context_instance=RequestContext(request))
 
 def survey(request, experiment):
-    pass
+	logger.debug("handling survey")
+	return render_to_response('sanitation/survey.html', locals(), context_instance=RequestContext(request))
 
 def quiz(request, experiment):
-    pass
+	quiz_questions = [('q1', 'This is question #1?','text','answer1'), ('q2', 'This is question #2?','text','answer2')]
+	logger.debug("handling quiz")
+	return render_to_response('sanitation/quiz.html', locals(), context_instance=RequestContext(request))
 
 def instructions(request, experiment):
-    pass
+	logger.debug("handling instructions")
+	return render_to_response('sanitation/instructions.html', locals(), context_instance=RequestContext(request))
 
 def play(request, experiment):
-    pass
+	logger.debug("handling play")
+	return render_to_response('sanitation/play.html', locals(), context_instance=RequestContext(request))
 
 def participate(request, experiment_id=None):
 # lookup participant's current location and then invoke the method named by the location
     participant = request.user.participant
-# FIXME: this isn't implemented
     experiment = Experiment.objects.get(pk=experiment_id)
-    current_location = participant.current_location # "consent", "survey", "quiz", "play", "instructions"
+# FIXME: this isn't implemented
+    current_location = "survey"
+#    current_location = participant.current_location # "consent", "survey", "quiz", "play", "instructions"
     if current_location in ["consent", "survey", "quiz", "play", "instructions"]:
         logger.debug("current location %s is valid", current_location)
 # invoke current_location as a method and pass in the request and the experiment
-        location_method = getattr(__name__, current_location)
+        location_method = globals()[current_location]
         return location_method(request, experiment)
     logger.debug("Invalid location %s, redirecting to dashboard", current_location)
-    return redirect('core:dashboard')
-
+#    return redirect('core:dashboard')
+    return render_to_response('sanitation/'+ current_location + '.html', {
+        'experiment': experiment,
+        },
+        context_instance=RequestContext(request))
