561adb926dd672fccba2183ba8ab445aa32866f8
Allen Lee <allen.lee@asu.edu>
2011-08-10 17:24:11 -0700

    updating forms to use shorter participant_group_id

--- vcweb/lighterprints/forms.py
+++ vcweb/lighterprints/forms.py
@@ -3,9 +3,9 @@ from django import forms
 
 class ChatForm(forms.Form):
     message = forms.CharField(required=True, max_length=512)
-    participant_group_relationship_id = forms.IntegerField(required=True, widget=forms.HiddenInput)
+    participant_group_id = forms.IntegerField(required=True, widget=forms.HiddenInput)
 
 class ActivityForm(forms.Form):
     activity_id = forms.IntegerField(required=True, widget=forms.HiddenInput)
-    participant_group_relationship_id = forms.IntegerField(required=True, widget=forms.HiddenInput)
+    participant_group_id = forms.IntegerField(required=True, widget=forms.HiddenInput)
 
