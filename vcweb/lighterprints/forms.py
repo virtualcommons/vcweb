3f0084e00c392f718a4db6c122c2fefab714e832
Allen Lee <allen.lee@asu.edu>
2011-08-12 01:10:11 -0700

    html escaping incoming chat messages

--- vcweb/lighterprints/forms.py
+++ vcweb/lighterprints/forms.py
@@ -1,9 +1,14 @@
 from django import forms
 
+from django.utils.html.escape import escape
+
 
 class ChatForm(forms.Form):
     message = forms.CharField(required=True, max_length=512)
     participant_group_id = forms.IntegerField(required=True, widget=forms.HiddenInput)
+    def clean_message(self):
+        return escape(self.cleaned_data['message'])
+
 
 class ActivityForm(forms.Form):
     activity_id = forms.IntegerField(required=True, widget=forms.HiddenInput)
