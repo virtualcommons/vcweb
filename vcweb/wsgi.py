2be1421b2a7b4779672406562489988523944f2b
Allen Lee <allen.lee@asu.edu>
2012-03-28 17:08:37 -0700

    switching to django 1.4 wsgi and fixing wsgi apache configuration
    - added the filesystem path to the vcweb codebase to WSGIDaemonProcess python-path
    - SSL VirtualHost needs to reference the WSGIProcessGroup created in the standard httpd.conf

--- vcweb/wsgi.py
+++ vcweb/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for django14_test project.
+WSGI config for vcweb project.
 
 This module contains the WSGI application used by Django's development server
 and any production WSGI deployments. It should expose a module-level variable
@@ -15,7 +15,7 @@ framework.
 """
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django14_test.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vcweb.settings")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
