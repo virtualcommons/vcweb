1d2c03c4f6b7abb1a3ddc0574f8637549b46157f
Allen Lee <allen.lee@asu.edu>
2012-04-05 00:20:26 -0700

    experimenting with memcached for lighterprints json views.  might be a bad idea

--- vcweb/lighterprints/urls.py
+++ vcweb/lighterprints/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import url, patterns
-from django.views.decorators.cache import never_cache
+from django.views.decorators.cache import cache_page
 
 from vcweb.lighterprints.views import (ActivityDetailView, ActivityListView, MobileView, post_chat_message,
         post_comment, perform_activity, DiscussionBoardView, login, participate, group_activity, like,
@@ -10,10 +10,10 @@ urlpatterns = patterns('vcweb.lighterprints.views',
     url(r'^mobile$', MobileView.as_view(), name='mobile'),
     url(r'^(?P<experiment_id>\d+)/configure$', 'configure', name='configure'),
     url(r'^((?P<experiment_id>\d+)/)?participate/?$', participate, name='participate'),
-    url(r'^activity/list/?$', never_cache(ActivityListView.as_view())),
-    url(r'^activity/(?P<activity_id>\d+)$', never_cache(ActivityDetailView.as_view())),
+    url(r'^activity/list/?$', cache_page(60)(ActivityListView.as_view())),
+    url(r'^activity/(?P<activity_id>\d+)$', cache_page(60*15)(ActivityDetailView.as_view())),
     url(r'^discussion/(?P<experiment_id>\d+)/(?P<participant_id>\d+)', DiscussionBoardView.as_view()),
-    url(r'^api/group-activity/(?P<participant_group_id>\d+)', never_cache(group_activity)),
+    url(r'^api/group-activity/(?P<participant_group_id>\d+)', cache_page(60)(group_activity)),
     url(r'^api/do-activity$', perform_activity),
     url(r'^api/message', post_chat_message),
     url(r'^api/comment', post_comment),
