5e32cfeb94633bfcdd4e09d5a409e718a8ae2143
Allen Lee <allen.lee@asu.edu>
2012-06-27 11:58:22 -0700

    minor visual tweaks to experimenter dashboard

--- fabfile.py
+++ fabfile.py
@@ -104,11 +104,9 @@ def tornadio(ip="127.0.0.1", port=None):
         port = vcweb_settings.SOCKET_IO_PORT
     _virtualenv(local, "{python} vcweb/vcwebio.py {port}".format(python=env.python, **locals()), capture=False)
 
+
 def server(ip="127.0.0.1", port=8000):
     local("{python} manage.py runserver {ip}:{port}".format(python=env.python, **locals()), capture=False)
-
-def celeryd():
-    local("%(python)s manage.py celeryd -B" % env)
 
 def push():
     local('hg push ssh://hg@bitbucket.org/virtualcommons/vcweb')
