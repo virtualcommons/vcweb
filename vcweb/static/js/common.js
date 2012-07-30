43b30dbf1531da2bad0a65dc58442868c8a781d1
Allen Lee <allen.lee@asu.edu>
2012-07-29 17:32:06 -0700

    - replacing qtip with bootstrap popovers ( http://twitter.github.com/bootstrap/javascript.html#popovers ) - more work on sockjs implementation

--- vcweb/static/js/common.js
+++ vcweb/static/js/common.js
@@ -6,26 +6,6 @@ console.info = console.info || function(){};
 function scrollToBottom(element) {
     element.scrollTop = element.scrollHeight;
 }
-function addChatMessage(elementId, json, title, qtipOptions) {
-    if (! title) {
-        title = json.date_created;
-    }
-    if (! qtipOptions) {
-        qtipOptions = {position: { corner: {target: 'topMiddle', tooltip: 'bottomMiddle'}}, style: { name: 'green', tip: 'bottomMiddle'} };
-    }
-    $('#' + elementId).append(
-        $("<div class='ui-state-highlight' style='line-height: 1.5em;'/>")
-        .append(
-            $("<a class='dark-yellow-highlight' />")
-            .attr("name", json.pk)
-            .attr("title", title)
-            .text(json.date_created)
-            .qtip(qtipOptions)
-        )
-        .append(" | ")
-        .append(json.message));
-    scrollToBottom(document.getElementById(elementId));
-}
 
 $(document).ajaxSend(function(event, xhr, settings) {
     function sameOrigin(url) {
