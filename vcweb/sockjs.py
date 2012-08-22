64cc9bb0fd21173fe4f4bffae2b64030b1f6c0d5
Allen Lee <allen.lee@asu.edu>
2012-08-22 16:29:52 -0700

    splitting up sockjs connections into experimenter and participant connections.  still need to work out session management and mapping to allow experimenters to send messages to all connected clients within their experiment

--- vcweb/sockjs.py
+++ vcweb/sockjs.py
@@ -6,6 +6,7 @@ import tornadoredis
 import simplejson as json
 from tornado import web, ioloop
 from sockjs.tornado import SockJSRouter, SockJSConnection
+from itertools import chain
 
 sys.path.append(os.path.abspath('.'))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'vcweb.settings'
@@ -15,10 +16,10 @@ from vcweb import settings
 
 logger = logging.getLogger('sockjs.vcweb')
 
-class VcwebConnection(SockJSConnection):
-    default_channel = 'vcweb.websocket'
+class ParticipantConnection(SockJSConnection):
+    default_channel = 'vcweb.participant.websocket'
     def __init__(self, *args, **kwargs):
-        super(VcwebConnection, self).__init__(*args, **kwargs)
+        super(ParticipantConnection, self).__init__(*args, **kwargs)
         self.client = tornadoredis.Client()
         #self.client.connect()
         #self.client.subscribe(self.default_channel)
@@ -39,16 +40,40 @@ class VcwebConnection(SockJSConnection):
         #self.client.unsubscribe(self.default_channel)
         pass
 
+class ExperimenterConnection(SockJSConnection):
+    default_channel = 'vcweb.experimenter.websocket'
+    def __init__(self, *args, **kwargs):
+        super(ExperimenterConnection, self).__init__(*args, **kwargs)
+        self.client = tornadoredis.Client()
+
+    def on_open(self, info):
+        logger.debug("opening connection %s", info)
+        #self.client.listen(self.on_chan_message)
+
+    def on_message(self, json_string):
+        logger.debug("received: " + json_string)
+        message_dict = json.loads(json_string)
+        logger.debug("received message %s", message_dict)
+        experiment_id = message_dict['experiment_id']
+        auth_token = message_dict['auth_token']
+        experiment = Experiment.objects.get(pk=experiment_id)
+
+    def on_close(self):
+        #self.client.unsubscribe(self.default_channel)
+        pass
+
+
 def main(argv=None):
     if argv is None:
         argv = sys.argv
     # currently only allow one command-line argument, the port to run on.
     logging.getLogger().setLevel(logging.DEBUG)
     port = int(argv[1]) if (len(argv) > 1) else settings.WEBSOCKET_PORT
-    uri = argv[2] if len(argv) > 2 else '/sockjs'
-    VcwebRouter = SockJSRouter(VcwebConnection, uri)
-    app = web.Application(VcwebRouter.urls)
-    logger.info("starting sockjs server [%s] on port %s", uri, port)
+    ParticipantRouter = SockJSRouter(ParticipantConnection, '/participant')
+    ExperimenterRouter = SockJSRouter(ExperimenterConnection, '/experimenter')
+    urls = list(chain.from_iterable([ParticipantRouter.urls, ExperimenterRouter.urls, ]))
+    app = web.Application(urls)
+    logger.info("starting sockjs server on port %s", port)
     app.listen(port)
     ioloop.IOLoop.instance().start()
 
