6351a6ed621ccafc2ba369dd6b60bfcdbe347107
Allen Lee <allen.lee@asu.edu>
2012-11-09 17:11:15 -0700

    adding canned data and temporary test select for picking which template to render

--- vcweb/boundaries/models.py
+++ vcweb/boundaries/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 from vcweb.core import simplecache
 from vcweb.core.models import ExperimentMetadata, Parameter, ParticipantRoundDataValue
-from vcweb.forestry.models import (get_harvest_decision_parameter, get_harvest_decision,
-        get_regrowth_rate, get_group_harvest_parameter)
+from vcweb.forestry.models import (get_harvest_decision_parameter, get_harvest_decision, get_regrowth_rate, get_group_harvest_parameter)
 
 
 @simplecache
@@ -15,9 +14,13 @@ def get_player_status_parameter():
 
 @simplecache
 def get_survival_cost_parameter():
-    return Parameter.objects.for_participant(name='survival_cost')
+    return Parameter.objects.for_round(name='survival_cost')
 
 # players can either be dead or alive
 def get_player_status_dv(participant_group_relationship_id):
     return ParticipantRoundDataValue.objects.get(parameter=get_player_status_parameter(), participant_group_relationship__pk=participant_group_relationship_id)
+
+def get_survival_cost(current_round):
+    return current_round.get_parameter_value('survival_cost', default=5)
+
 
--- vcweb/boundaries/views.py
+++ vcweb/boundaries/views.py
@@ -9,7 +9,7 @@ from vcweb.core.decorators import participant_required
 from vcweb.core.json import dumps
 from vcweb.core.models import is_participant, is_experimenter, Experiment
 from vcweb.core.views import ParticipantSingleExperimentMixin
-from vcweb.boundaries.models import get_experiment_metadata, get_regrowth_rate
+from vcweb.boundaries.models import get_experiment_metadata, get_regrowth_rate, get_survival_cost
 import logging
 
 logger = logging.getLogger(__name__)
@@ -22,17 +22,35 @@ def participate(request, experiment_id=None):
     if experiment.experiment_metadata != get_experiment_metadata():
         raise Http404
     participant_experiment_relationship = participant.get_participant_experiment_relationship(experiment)
+    number_of_trees_per_row = 20
+    max_width = number_of_trees_per_row * 30
+    tree = trees['pine']
+    resource_level = 100
+    max_height = (resource_level / number_of_trees_per_row) * tree['height']
+    number_of_resource_divs = range(0, resource_level / number_of_trees_per_row)
+    resource_width = (resource_level % number_of_trees_per_row) * 30
 # FIXME: this should always render the participate.html template and expose a
 # JSON RoundConfiguration object to the page so the template knows what to render..?
     return render_to_response('boundaries/participate.html', {
         'auth_token': participant.authentication_token,
         'participant_experiment_relationship': participant_experiment_relationship,
         'experiment': experiment,
-        'experimentModelJson': to_json(experiment),
+        'max_width': max_width,
+        'max_height': max_height,
+        'experimentModelJson': to_json(experiment, participant),
         },
         context_instance=RequestContext(request))
 
-def to_json(experiment):
+
+# FIXME: figure out the appropriate place for this
+trees = {
+        'deciduous': { 'name': 'deciduous-tree', 'height': 32 },
+        'pine': {'name': 'pine-tree', 'height': 79 },
+        }
+
+def to_json(experiment, participant):
+    pgr = participant.get_participant_group_relationship(experiment)
+    group = pgr.group
     ec = experiment.experiment_configuration
     current_round = experiment.current_round
     experiment_model_dict = experiment.as_dict(include_round_data=False, attrs={})
@@ -40,5 +58,10 @@ def to_json(experiment):
     experiment_model_dict['numberOfRounds'] = ec.final_sequence_number
     experiment_model_dict['roundType'] = current_round.round_type
     experiment_model_dict['regrowthRate'] = get_regrowth_rate(current_round)
+    experiment_model_dict['survivalCost'] = get_survival_cost(current_round)
+# FIXME: hard coded for now
+    experiment_model_dict['maxHarvestDecision'] = 10
+    experiment_model_dict['storedResources'] = 20
+    experiment_model_dict['resourceLevel'] = 100
+    experiment_model_dict['alreadySubmittedDecision'] = False
     return dumps(experiment_model_dict)
-
