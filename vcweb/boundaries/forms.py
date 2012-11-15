6d0fa1d81c3edcdfa0622d08758ccaa1d7f2c371
Allen Lee <allen.lee@asu.edu>
2012-11-15 04:16:07 -0700

    adding boundaries forms api - simple wrapper around forestry for the moment

---
+++ vcweb/boundaries/forms.py
@@ -0,0 +1,7 @@
+from vcweb.forestry import forms
+
+
+class HarvestDecisionForm(forms.HarvestDecisionForm):
+# XXX: only used to export HarvestDecisionForm in its own namespace at the moment
+    pass
+
