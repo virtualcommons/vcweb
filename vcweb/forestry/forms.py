183798feea7b13173070dba031678359ea9f798a
Allen Lee <allen.lee@asu.edu>
2012-11-15 03:43:33 -0700

    removing unneeded code as IntegerField already does type checking for us

--- vcweb/forestry/forms.py
+++ vcweb/forestry/forms.py
@@ -1,16 +1,8 @@
 from django import forms
+import logging
 
-REQUIRED_ATTRIBUTES = { 'class' : 'required' }
+logger = logging.getLogger(__name__)
 
 class HarvestDecisionForm(forms.Form):
-    harvest_decision = forms.IntegerField(required=True, min_value=0, max_value=5)
+    harvest_decision = forms.IntegerField(required=True, min_value=0)
     participant_group_id = forms.IntegerField(required=True, widget=forms.widgets.HiddenInput)
-
-    def clean(self):
-        harvest_decision = self.cleaned_data['harvest_decision']
-        try:
-            # only testing for integer-ness
-            int(harvest_decision)
-            return self.cleaned_data
-        except:
-            raise forms.ValidationError("Invalid harvest decision %s" % harvest_decision)
