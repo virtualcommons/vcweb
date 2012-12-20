87e1ad5939505db6d87adbc7a194ddae39c8e543
Allen Lee <allen.lee@asu.edu>
2012-12-20 15:11:26 -0700

    extracting model to its own JS file and stripping out most full webapp DOM-specific elements

---
+++ vcweb/static/js/lighterprints/model.js
@@ -0,0 +1,27 @@
+function LighterFootprintsModel(modelJson) {
+    var self = this;
+    var model = ko.mapping.fromJS(modelJson);
+    // FIXME: hacky, figure out if there is a way to pass the observable in directly from the model object we get in
+    // performActivity
+    model.lastPerformedActivity = ko.observable();
+    model.lastPerformedActivityPoints = ko.observable();
+    model.errorMessage = ko.observable();
+    model.hasChatMessages = function() {
+        return model.chatMessages().length > 0;
+    }
+    model.submitChatMessage = function() {
+        var formData = $('#chat-form').serialize();
+        $.post('/lighterprints/api/message', formData, function(data) {
+                ko.mapping.fromJS(data, model);
+            });
+        $('#chatText').val('');
+        return false;
+    };
+    model.availableActivities = ko.computed(function() {
+            return ko.utils.arrayFilter(model.activities(), function(activity) { return activity.available_now() });
+    });
+    model.hasAvailableActivities = ko.computed(function() {
+            return model.availableActivities().length > 0;
+    });
+    return model;
+}
