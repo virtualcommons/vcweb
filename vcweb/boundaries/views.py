165f67e299368fd229e221c326b79f3e0d92ee27
Allen Lee <allen.lee@asu.edu>
2013-01-16 16:18:48 -0700

    initial changes to boundaries experiment interface

--- vcweb/boundaries/views.py
+++ vcweb/boundaries/views.py
@@ -68,6 +68,7 @@ def to_json(experiment, participant, **kwargs):
     experiment_model_dict['participantNumber'] = pgr.participant_number
     experiment_model_dict['participantGroupId'] = pgr.pk
 # FIXME: defaults hard coded in for now
+    experiment_model_dict['lastHarvestDecision'] = 5
     experiment_model_dict['maxHarvestDecision'] = 10
     experiment_model_dict['storage'] = 20
     experiment_model_dict['resourceLevel'] = 100
