fce013650741402f80960635f5ab568b97ae17f7
Allen Lee <allen.lee@asu.edu>
2013-01-22 17:01:29 -0700

    removing participant

--- vcweb/broker/views.py
+++ vcweb/broker/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import get_object_or_404, render, redirect
 
 def participate(request, experiment_id=None):
-    participant = request.user.participant
+    #participant = request.user.participant
     return render(request, 'broker/participate.html', {
-        'participant': participant,
+    #    'participant': participant,
 
         })
