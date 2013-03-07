2e68e8347e69d05b779c8b98a715bcbc04920c7c
alllee <allen.lee@asu.edu>
2013-03-06 23:19:18 -0700

    adding dev server warning when DEBUG is enabled

--- vcweb/core/context_processors.py
+++ vcweb/core/context_processors.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 
 def debug_mode(request):
-    return {'debug_mode': settings.DEBUG}
+    return {'DEBUG': settings.DEBUG}
 
 def websocket(request):
     return {'WEBSOCKET_PORT' : settings.WEBSOCKET_PORT}
