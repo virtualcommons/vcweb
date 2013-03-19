a63d26ba9ec545a2fdaa7b063bfb0498275e0d67
alllee <allen.lee@asu.edu>
2013-03-18 17:06:54 -0700

    adding chat_enabled column and migration

--- vcweb/core/models.py
+++ vcweb/core/models.py
@@ -918,6 +918,7 @@ class RoundConfiguration(models.Model):
                         e.g., if set to foo.html, vcweb will look for templates/forestry/foo.html'''))
     survey_url = models.URLField(null=True, blank=True)
     """ external survey url """
+    chat_enabled = models.BooleanField(default=False, help_text=_("Is chat enabled in this round?"))
     randomize_groups = models.BooleanField(default=False, help_text=_("Shuffle participants into new groups when the round begins?"))
     """ Should groups be randomized at the start of the round? """
     preserve_existing_groups = models.BooleanField(default=True, help_text=_("This option is only useful if randomize_groups is set to true.  If we are randomizing groups, should existing groups (if any) be preserved?"))
