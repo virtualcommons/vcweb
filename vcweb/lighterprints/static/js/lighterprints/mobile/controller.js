03a3daa222b05692953dc0369184c430a6138c39
RoleeSinha <none@none>
2013-05-06 17:40:58 -0700

    fixing ui

--- vcweb/lighterprints/static/js/lighterprints/mobile/controller.js
+++ vcweb/lighterprints/static/js/lighterprints/mobile/controller.js
@@ -45,7 +45,9 @@ function LighterFootprintsModel(modelJson){
             return;
         }
         var id = challengeModel.pk();
-        var formData = $('#challengeForm' + id).serialize();
+        var formData = {
+			participant_group_id: participant_group_id, activity_id:id
+		};
         $.post('/lighterprints/api/do-activity', formData, function(data){
             if (data.success) {
 				console.debug(data);
