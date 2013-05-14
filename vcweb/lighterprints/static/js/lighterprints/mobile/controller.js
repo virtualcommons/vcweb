edf7658e567a0b9f4080477744811150baf8c3f3
RoleeSinha <none@none>
2013-05-14 14:39:34 -0700

    fixing KO binding

--- vcweb/lighterprints/static/js/lighterprints/mobile/controller.js
+++ vcweb/lighterprints/static/js/lighterprints/mobile/controller.js
@@ -39,59 +39,6 @@ function LighterFootprintsModel(modelJson){
         return model.availableActivities().length > 0;
     });
     model.lastPerformedActivity = ko.observable();
-    
-    model.perform = function(challengeModel){
-        if (!challengeModel.availableNow()) {
-            console.debug("tried to perform an activity that's not available right now");
-            console.debug(challengeModel);
-            return;
-        }
-        var id = challengeModel.pk();
-        var formData = {
-            participant_group_id: model.participantGroupId(),
-            activity_id: id
-        };
-        $.post('/lighterprints/api/do-activity', formData, function(data){
-            if (data.success) {
-                console.debug(data);
-                ko.mapping.fromJSON(data.viewModel, model);
-                model.lastPerformedActivity(challengeModel);
-				ko.applyBindings(globalViewModel);
-                $("#activityCompletedDialog").popup('open');
-                console.debug('refreshing lists');
-                $('#challengesList').listview('refresh');
-                $('#teamList').listview('refresh');
-                $('#teamActivityList').listview('refresh');
-            }
-            else {
-                console.debug("ERROR: " + data.message);
-            }
-        });
-    };
-    
-    model.send = function(){
-    
-        if ($('#chatText').val()) {
-            var formData = {
-                participant_group_id: model.participantGroupId(),
-                message: $('#chatText').val()
-            };
-            $.post('/lighterprints/api/message', formData, function(response){
-                if (response.success) {
-                    console.debug("successful post - updated view model: ");
-                    ko.mapping.fromJS(response.viewModel, model);
-					ko.applyBindings(globalViewModel);
-                    console.debug('refresh chatMessage List');
-                    $('#chatMessageList').listview('refresh');
-                }
-                else {
-                    console.debug("unable to post message to server");
-                    console.debug(response);
-                }
-            });
-            $('#chatText').val('');
-        }
-    };
-    
+  
     return model;
 }
