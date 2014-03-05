9a1411a02dd947ca5c55f8272e9cef932a313d61
Allen Lee <allen.lee@asu.edu>
2014-03-04 17:06:15 -0700

    adding test query

--- vcweb/subject_pool/management/commands/migratespool.py
+++ vcweb/subject_pool/management/commands/migratespool.py
@@ -14,4 +14,11 @@ class Command(BaseCommand):
         # open db connection to subject pool
         cnx = mysql.connector.connect(user='spool', password='spool.migration', host='127.0.0.1', database='spool')
         self.stdout.write("opened connection %s to spool database" % cnx)
+        cursor = cnx.cursor()
+        query = ("SELECT u.username, u.fullName, u.dateCreated, u.emailAddress, p.gender, p.classStatus, p.major, p.affiliation "
+                " FROM user u inner join participant p on u.id=p.id "
+                " WHERE u.active=1 LIMIT 10")
+        cursor.execute(query)
+        for (username, full_name, date_created, email, gender, class_status, major, affiliation) in cursor:
+            self.stdout.write("processing {} {}".format(username, email))
         cnx.close()
