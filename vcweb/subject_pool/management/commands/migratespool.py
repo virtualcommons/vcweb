a12e38d1d4fdf36ef0c43f22b94d5679831e6e0b
Allen Lee <allen.lee@asu.edu>
2014-03-07 14:47:38 -0700

    adding exclusion logic based on class status and registration date

--- vcweb/subject_pool/management/commands/migratespool.py
+++ vcweb/subject_pool/management/commands/migratespool.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 from django.core.management.base import BaseCommand, CommandError
 from django.contrib.auth.models import User
 from vcweb.core.models import Participant, Institution, set_full_name
@@ -21,15 +22,39 @@ class Command(BaseCommand):
                 " WHERE u.active=1")
         cursor.execute(query)
         asu_institution = Institution.objects.get(name='Arizona State University')
+        existing_users = []
         users = []
         participants = []
+        skipped_participants = []
+        class_status_offsets = {'Freshman': timedelta(days=365*3), 'Sophomore': timedelta(days=365*2), 'Junior': timedelta(days=365)}
+        now = datetime.now()
+
         for (username, full_name, date_created, email, gender, class_status, major, affiliation) in cursor:
-            u = User(username=username, email=email, password=User.objects.make_random_password())
-            set_full_name(u, full_name)
-            users.append(u)
-            p = Participant(user=u, can_receive_invitations=True, gender=gender, major=major,
-                    class_status=class_status, date_created=date_created, institution=asu_institution)
-            self.stdout.write(u"processing {} {} {}, created {}".format(full_name, username, email, p))
-            participants.append(p)
-        self.stdout.write("participants: {} with users: {}".format(len(participants), len(users)))
+            if class_status == 'Senior':
+                self.stdout.write("skipping seniors {}".format(email))
+                skipped_participants.append("%s %s" % (username, email))
+                continue
+            else:
+                self.stdout.write("checking delta for class %s" % class_status)
+                delta = class_status_offsets[class_status]
+                if date_created + delta < now:
+                    # this one is expired
+                    self.stdout.write("Skipping {} that registered on {}".format(class_status, date_created))
+                    skipped_participants.append("%s %s" % (username, email))
+            #self.stdout.write(u"Looking at {} {} class {}".format(username, email, class_status))
+            try:
+                u = User.objects.get(email=email)
+                self.stdout.write(u"user already exists: {}".format(u))
+                existing_users.append(u)
+            except User.DoesNotExist:
+                u = User(username=username, email=email, password=User.objects.make_random_password())
+                users.append(u)
+                set_full_name(u, full_name)
+                p = Participant(user=u, can_receive_invitations=True, gender=gender, major=major,
+                        class_status=class_status, date_created=date_created, institution=asu_institution)
+                self.stdout.write(u"processing {} {} {}, created {}".format(full_name, username, email, p))
+                participants.append(p)
+        self.stdout.write("new participants: {}, existing users: {}".format(len(participants), len(existing_users)))
+        self.stdout.write("existing users: %s" % existing_users)
+        self.stdout.write("skipped users: %s" % skipped_participants)
         cnx.close()
