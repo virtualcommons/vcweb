5b499f6a7a6999f3efa6201f620c8b93be525ab7
alllee <allen.lee@asu.edu>
2014-05-06 01:16:25 -0700

    using 3 tuple include for namespacing

--- vcweb/core/urls.py
+++ vcweb/core/urls.py
@@ -83,22 +83,23 @@ urlpatterns = [
     # subject pool urls
     url(r'^subject-pool/$', subject_pool_index, name='subject_pool_index'),
     url(r'^subject-pool/',
-        include([
-                    url(r'^session/update$', update_session, name='update_session'),
-                    url(r'^session/events$', get_session_events, name='session_events'),
-                    url(r'^session/detail/event/(\d+)$', manage_participant_attendance, name='session_event_detail'),
-                    url(r'^session/invite$', send_invitations, name='send_invites'),
-                    url(r'^session/invite/count$', get_invitations_count, name='get_invitations_count'),
-                    url(r'^session/attendance$', manage_participant_attendance, name='participant_attendance'),
-                    url(r'^session/email-preview$', invite_email_preview, name='invite_email_preview'),
-                    url(r'^signup/$', experiment_session_signup, name='experiment_session_signup'),
-                    url(r'^signup/submit/$', submit_experiment_session_signup, name='submit_experiment_session_signup'),
-                    url(r'^signup/cancel/$', cancel_experiment_session_signup, name='cancel_experiment_session_signup'),
-                    url(r'^session/(?P<pk>\d+)/download/$', download_experiment_session,
-                        name='download_experiment_session'),
+        include((
+            [
+                url(r'^session/update$', update_session, name='update_session'),
+                url(r'^session/events$', get_session_events, name='session_events'),
+                url(r'^session/detail/event/(\d+)$', manage_participant_attendance, name='session_event_detail'),
+                url(r'^session/invite$', send_invitations, name='send_invites'),
+                url(r'^session/invite/count$', get_invitations_count, name='get_invitations_count'),
+                url(r'^session/attendance$', manage_participant_attendance, name='participant_attendance'),
+                url(r'^session/email-preview$', invite_email_preview, name='invite_email_preview'),
+                url(r'^signup/$', experiment_session_signup, name='experiment_session_signup'),
+                url(r'^signup/submit/$', submit_experiment_session_signup, name='submit_experiment_session_signup'),
+                url(r'^signup/cancel/$', cancel_experiment_session_signup, name='cancel_experiment_session_signup'),
+                url(r'^session/(?P<pk>\d+)/download/$', download_experiment_session,
+                    name='download_experiment_session'),
                 ],
-                namespace='subject_pool',
-                app_name='subject_pool')),
+            'subject_pool',
+            'subject_pool'))),
 ]
 
 
