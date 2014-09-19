cdbbf3263e9df5742de1b791d556b1f041810a98
Allen Lee <allen.lee@asu.edu>
2014-09-19 14:05:42 -0700

    pruning dead code

--- vcweb/experiment/bound/forms.py
+++
@@ -1,7 +0,0 @@
-from vcweb.experiment.forestry import forms
-
-
-class HarvestDecisionForm(forms.HarvestDecisionForm):
-    # XXX: only used to export HarvestDecisionForm in its own namespace at the
-    # moment
-    pass
