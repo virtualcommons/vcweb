c9b2a4ce474166ee0e75098cb8b7dc3a43ede43a
tgpatel <mascottejas@gmail.com>
2014-09-29 14:31:06 -0700

    Adding logging code to sockjs

--- vcweb/vcweb-sockjs-redis.py
+++ vcweb/vcweb-sockjs-redis.py
@@ -13,18 +13,63 @@ from sockjs.tornado import SockJSRouter, SockJSConnection
 from tornado import web, ioloop
 from raven.contrib.tornado import AsyncSentryClient
 
-from django.conf import settings
+from logging.config import dictConfig
 
 from redis_pubsub import RedisPubSub
 
-# redefine logger
-logger = logging.getLogger(__name__)
+from django.conf import settings
 
 sys.path.append(
     path.abspath(path.join(path.dirname(path.abspath(__file__)), '..')))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'vcweb.settings.local'
 
+TORNADO_LOG_FILENAME = "vcweb-tornado.log"
+TORNADO_LOG = path.join(settings.LOG_DIRECTORY, TORNADO_LOG_FILENAME)
+
 DEFAULT_WEBSOCKET_PORT = 8882
+
+dictConfig({
+    'version': 1,
+    'disable_existing_loggers': True,
+    'root': {
+        'level': 'DEBUG',
+        'handlers': ['tornado.file', 'console'],
+    },
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s [%(name)s|%(funcName)s:%(lineno)d] %(message)s'
+        }
+    },
+    'loggers': {
+        'vcweb': {
+            'level': 'DEBUG',
+            'handlers': ['tornado.file', 'console'],
+            'propagate': False,
+        },
+        'sockjs.vcweb': {
+            'level': 'DEBUG',
+            'handlers': ['tornado.file', 'console'],
+            'propagate': False,
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+        },
+        'tornado.file': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'formatter': 'verbose',
+            'filename': TORNADO_LOG,
+            'backupCount': 6,
+            'maxBytes': 10000000,
+        },
+    },
+})
+
+logger = logging.getLogger('sockjs.vcweb')
 
 # Create the tornadoredis.Client instance and use it for redis channel subscriptions
 subscriber = tornadoredis.pubsub.SockJSSubscriber(tornadoredis.Client())
@@ -98,7 +143,6 @@ def main(argv=None):
     app = web.Application(urls)
 
     logger.info("starting sockjs server on port %s", port)
-    print "starting sockjs server on port " + str(port)
 
     app.listen(port)
 
