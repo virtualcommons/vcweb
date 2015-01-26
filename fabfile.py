71200b4391804e8af5a4a4531589b27c0e3fba35
Allen Lee <allen.lee@asu.edu>
2015-01-26 13:19:48 -0700

    add migrate to confirmable deploy steps

--- fabfile.py
+++ fabfile.py
@@ -202,6 +202,8 @@ def deploy(vcs_branch_dict):
                 _virtualenv(sudo, 'pip install -Ur requirements.txt', user=env.deploy_user)
             if confirm("Update cron?"):
                 _virtualenv(sudo, '%(python)s manage.py installtasks' % env, user=env.deploy_user)
+            if confirm("Run database migrations?"):
+                _virtualenv(sudo, '%(python)s manage.py migrate' % env, user=env.deploy_user)
             _virtualenv(sudo, '%(python)s manage.py collectstatic' % env, user=env.deploy_user)
             sudo_chain(
                 'chmod -R ug+rw .',
