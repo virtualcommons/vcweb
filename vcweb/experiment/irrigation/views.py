733a58de4a1eaedfedec6a8ab7bf1a6307f07136
Allen Lee <allen.lee@asu.edu>
2015-02-06 14:21:02 -0700

    fix import errors

--- vcweb/experiment/irrigation/views.py
+++ vcweb/experiment/irrigation/views.py
@@ -3,7 +3,7 @@ import logging
 from django.shortcuts import render, get_object_or_404
 
 from vcweb.core.decorators import group_required
-from vcweb.core.http import JsonResponse, dumps, Http404
+from vcweb.core.http import JsonResponse, dumps
 from vcweb.core.models import Experiment, PermissionGroup
 from vcweb.experiment.irrigation.services import get_experiment_metadata
 
@@ -25,14 +25,14 @@ class ViewModel(object):
         self.experiment = self.group.experiment if experiment is None else experiment
         self.current_round_data = self.experiment.current_round_data
         self.current_round = self.current_round_data.round_configuration
-        self.experiment_model = self.experiment.to_dict(
-            include_round_data=False, default_value_dict=ViewModel.experiment_model_defaults)
+        self.experiment_model = self.experiment.to_dict(include_round_data=False,
+                                                        default_value_dict=ViewModel.experiment_model_defaults)
 
     def to_dict(self):
         current_round = self.experiment.current_round
         self.experiment_model.update(
             pid=self.participant_group_relationship.pk,
-            templateName=self.current_round.template_name,
+            templateName=current_round.template_name,
         )
         return self.experiment_model
 
@@ -43,11 +43,10 @@ class ViewModel(object):
 @group_required(PermissionGroup.participant, PermissionGroup.demo_participant)
 def participate(request, experiment_id=None):
     participant = request.user.participant
-    experiment = get_object_or_404(Experiment.objects.select_related(
-        'experiment_metadata', 'experiment_configuration'), pk=experiment_id)
+    experiment = get_object_or_404(Experiment.objects.select_related('experiment_metadata', 'experiment_configuration'),
+                                   pk=experiment_id,
+                                   experiment_metadata=get_experiment_metadata())
     pgr = experiment.get_participant_group_relationship(participant)
-    if experiment.experiment_metadata != get_experiment_metadata():
-        raise Http404
     return render(request, experiment.participant_template,
                   experiment.template_context(pgr, experimentModelJson=ViewModel(pgr, experiment=experiment).to_json()))
 
