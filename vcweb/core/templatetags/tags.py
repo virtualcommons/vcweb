892f1625e56b75f3fb9b5613690ccb513ef149a0
Allen Lee <allen.lee@asu.edu>
2015-03-19 11:41:19 -0700

    refactor template tags

--- vcweb/core/templatetags/tags.py
+++ vcweb/core/templatetags/tags.py
@@ -1,38 +1,21 @@
-# tags.py
-# custom template tags
-# active tag taken from
-# http://gnuvince.wordpress.com/2007/09/14/a-django-template-tag-for-the-current-active-page/
-
-import os
+from django import template
+import logging
 import re
 
-from django import template
-from django.conf import settings
-from django.contrib.auth.models import Group
-
-register = template.Library()
-
-import logging
-
 logger = logging.getLogger(__name__)
+register = template.Library()
 
 
 @register.simple_tag
 def active(request, pattern):
-    if request:
-        return 'active' if pattern == request.path else 'inactive'
-    else:
-        return 'inactive'
+    return 'active' if pattern == request.path else ''
 
 
 @register.simple_tag
 def active_re(request, pattern):
-    if request:
-        return 'active' if re.search(pattern, request.path) else 'inactive'
-    return 'inactive'
+    return 'active' if re.search(pattern, request.path) else ''
 
 
 @register.filter(name='has_group')
 def has_group(user, group_name):
-    group = Group.objects.get(name=group_name)
-    return True if group in user.groups.all() else False
+    return user.groups.filter(name=group_name).exists()
