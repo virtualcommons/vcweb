d76a1efc9e34be4ef0c03c0644487b937573a821
tgpatel <mascottejas@gmail.com>
2015-03-23 12:39:02 -0700

    Adding code to include invalid users to the audit email. Address Issue #24

--- vcweb/core/models.py
+++ vcweb/core/models.py
@@ -3241,10 +3241,13 @@ def weekly_schedule_tasks(sender, start=None, **kwargs):
     2) generate weekly activity log email with aggregate stats on participant signups,
        experiment status changes (run or archived), invitations sent
     """
-    # FIXME add information about invalid uses who are experimenter as well as participant.
     # FIXME add information regarding experiment status changes
     invalid_permission_participants = Participant.objects.active().exclude(user__groups__name=PermissionGroup.participant)
     invalid_permission_experimenters = Experimenter.objects.filter(user__is_active=True).exclude(user__email__contains=('mailinator.com')).exclude(user__groups__name=PermissionGroup.experimenter)
+
+    # invalid users who are experimenter as well as participant
+    participant_users = Participant.objects.active().values_list('user__id', flat=True)
+    invalid_users = Experimenter.objects.filter(user__id__in=participant_users)
 
     last_week_datetime = datetime.now() - timedelta(days=7)
     # participant signup in last week
@@ -3253,13 +3256,10 @@ def weekly_schedule_tasks(sender, start=None, **kwargs):
     # Invitations sent in last week
     invites_last_week = Invitation.objects.filter(date_created__gt=last_week_datetime).count()
 
-    email = create_markdown_email(template="email/audit-email.txt",
-                                  context={
-                                      "participants": invalid_permission_participants,
-                                      "experimenters": invalid_permission_experimenters,
-                                      "signups": signup_last_week,
-                                      "invites": invites_last_week
-                                    },
-                                  subject="VCWEB Audit",
-                                  to_email=[settings.DEFAULT_EMAIL])
+    email = create_markdown_email(template="email/audit-email.txt", context={
+                                    "invalid_users": invalid_users,
+                                    "participants": invalid_permission_participants,
+                                    "experimenters": invalid_permission_experimenters,
+                                    "signups": signup_last_week, "invites": invites_last_week
+                                 }, subject="VCWEB Audit", to_email=[settings.DEFAULT_EMAIL])
     mail.get_connection().send_messages([email,])
