d2303e235ab8b41abb8c46dd20965cd43b95da99
Allen Lee <allen.lee@asu.edu>
2016-04-13 17:15:43 -0700

    improve student class status validator
    - add --output argument
    - automatically deactivate invalid ASU students

--- vcweb/core/management/commands/validate_student_class_status.py
+++ vcweb/core/management/commands/validate_student_class_status.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
 from django.conf import settings
+from django.db import transaction
 from vcweb.core.models import Participant, ASUWebDirectoryProfile, create_markdown_email
 
 import unicodecsv as csv
@@ -11,26 +12,30 @@ logger = logging.getLogger(__name__)
 class Command(BaseCommand):
     help = 'Validates ASU Participant class statuses for the subject pool'
 
-    def handle(self, *args, **options):
-        participants = Participant.objects.select_related('user').filter(institution__name='Arizona State University')
-        invalid_participants = []
+    def add_arguments(self, parser):
+        parser.add_argument('--output', dest='output', default='/tmp/invalid-students.csv',
+                            help='Output file for participant status data culled from the ASU Web Directory')
 
-        filename = '/tmp/invalid_students.csv'
+    def handle(self, *args, **options):
+        active_participants = Participant.objects.active(institution__name='Arizona State University')
+        filename = options['output']
+        invalid_participants = []
+        for participant in active_participants:
+            # naive check to see if the participant's username has been set to their ASURITE
+            if participant.email != participant.username:
+                directory_profile = ASUWebDirectoryProfile(participant.username)
+                if directory_profile.profile_data is None or not directory_profile.is_undergraduate:
+                    invalid_participants.append(participant)
+                    participant.is_active = False
+                    participant.save()
         with open(filename, 'wb') as csvfile:
             writer = csv.writer(csvfile, delimiter=',')
             writer.writerow(['PK', 'Username', 'Email', 'Date Joined', 'Class Status'])
-
-            logger.debug("Checking permissions for total %s participants", participants.count())
-
-            for participant in participants:
-                if participant.email != participant.username:
-                    directory_profile = ASUWebDirectoryProfile(participant.username)
-                    if directory_profile.profile_data is None or not directory_profile.is_undergraduate:
-                        invalid_participants.append(participant)
-                        writer.writerow([participant.pk, participant.username, participant.email,
-                                         participant.user.date_joined.strftime("%m-%d-%Y %H:%M"),
-                                         participant.class_status])
-
+            for participant in invalid_participants:
+                writer.writerow([participant.pk, participant.username, participant.email,
+                                 participant.user.date_joined.strftime("%m-%d-%Y %H:%M"),
+                                 participant.class_status])
+        logger.debug("Checked permissions for %s participants", active_participants.count())
         email = create_markdown_email(template="email/monthly-audit-email.txt",
                                       context={'participants': invalid_participants},
                                       subject="VCWEB Monthly Audit", to_email=[settings.DEFAULT_EMAIL])
