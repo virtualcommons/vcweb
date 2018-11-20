c0e06e9df327b1d2560dc350a8eb67c5f4f890ba
Allen Lee <allen.lee@asu.edu>
2018-11-19 17:16:56 -0700

    use ./docker/shared instead of named volumes

--- deploy/runit/dev.sh
+++ deploy/runit/dev.sh
@@ -3,17 +3,6 @@
 CLEAN_DATABASE=${CLEAN_DATABASE:-"false"}
 DJANGO_SETTINGS_MODULE="vcweb.settings.dev"
 
-chmod a+x /code/deploy/*.sh
+chmod a+x /code/deploy/runit/*.sh
 
-initdb() {
-    cd /code;
-    if [ "$CLEAN_DATABASE" = "true" ]; then
-        echo "Destroying and initializing database from scratch"
-        /code/deploy/wait-for-it.sh db:5432 -- invoke db.init --clean
-    else
-        echo "Using existing db schema"
-        /code/deploy/wait-for-it.sh db:5432 -- invoke db.init
-    fi
-}
-initdb
-exec /code/manage.py runserver 0.0.0.0:8000
+exec /code/deploy/runit/wait-for-it.sh db:5432 -- /code/manage.py runserver 0.0.0.0:8000
