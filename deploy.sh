15910bd97b6d4ff9c5f253da0e8234df55282574
Allen Lee <allen.lee@asu.edu>
2018-11-26 16:09:33 -0700

    regenerate docker-compose.yml on deploy

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,11 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-ENVIRONMENT=${1:-"dev"}
+ENVIRONMENT=${1:-"dev"} # dev | staging | prod
 DJANGO_CONTAINER=${2:-"django"}
 
 echo "Deploying latest build of vcweb for **${ENVIRONMENT}**"
+./compose ${ENVIRONMENT}
 git describe --tags >| release-version.txt;
 docker-compose build --pull;
 docker-compose up -d;
