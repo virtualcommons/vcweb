2f4191713c6ab7ea956d8fe92ada2e13b223b931
Allen Lee <allen.lee@asu.edu>
2019-03-22 21:30:57 -0700

    update dependent docker images during deploy

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,7 @@ DJANGO_CONTAINER=${2:-"django"}
 echo "Deploying latest build of vcweb for **${ENVIRONMENT}**"
 ./compose ${ENVIRONMENT}
 git describe --tags >| release-version.txt;
+docker-compose pull db redis nginx;
 docker-compose build --pull;
 docker-compose up -d;
 docker-compose exec ${DJANGO_CONTAINER} inv prepare
